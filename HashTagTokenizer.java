18a099b559fe03791407087784da0731d1d345d0
Yuval Ben-Zvi <yuval.benzvi@post.runi.ac.il>
2024-02-05 11:23:45 +0200

    hw7

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -11,30 +11,41 @@ public class HashTagTokenizer {
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
-
 		In in = new In(fileName);
-
-		// Your code here
-
+		for (int i = 0; i < dictionary.length; i++) {
+			String str = in.readLine();
+			dictionary[i] = str;
+		}
 		return dictionary;
 	}
 
 	public static boolean existInDictionary(String word, String []dictionary) {
-		// Your code here
+		for (int i = 0; i < dictionary.length ; i++) {
+			if (dictionary[i].equals(word)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
-
 		// Base case: do nothing (return) if hashtag is an empty string.
         if (hashtag.isEmpty()) {
             return;
         }
- 
+		hashtag = hashtag.toLowerCase();
         int N = hashtag.length();
-
+		String word = "";
         for (int i = 1; i <= N; i++) {
-		
-        }
-    }
-
+			word = hashtag.substring(0, i);
+			if (existInDictionary(word, dictionary)) {
+				System.out.println(word);
+				if (i != N) {
+					hashtag = hashtag.substring(i);
+					breakHashTag(hashtag, dictionary);
+				}
+				break;
+       		}
+		}
+	}
 }
